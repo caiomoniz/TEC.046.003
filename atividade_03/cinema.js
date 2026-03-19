78ddb0c3b6485fd57162bd95424302b6f30b19bb
caiomoniz <caiobarbosamoniz@gmail.com>
2026-03-19 11:43:41 -0300

    arrays bidimensionais

---
+++ atividade_03/cinema.js
@@ -0,0 +1,11 @@
+let assentos = [
+    ["L", "L", "O"],
+    ["L", "O", "L"],
+    ["O", "L", "L"]
+];
+
+let cadeiraMeio = assentos[1][1];
+    console.log("A cadeira do meio está " + cadeiraMeio);
+    assentos[0][0] = "0";
+    console.log("O assento [0][0]) está " + cadeiraMeio);
+
