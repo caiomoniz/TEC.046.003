0d84be2cce10eef9b4545f80adeb329fc745c2d4
Caio Moniz <caiobarbosamoniz@gmail.com>
2026-05-26 12:17:10 -0300

    barra

--- atividade_rpg/javascript.js
+++ atividade_rpg/javascript.js
@@ -6,6 +6,33 @@ class Personagem {
         this.mana = mana;
         this.energia = energia;
     }
+    hero_atacar(alvo, habilidade) {
+
+        if (this.mana >= habilidade.custo
+            && this.energia >= habilidade.energia) {
+            alvo.hp = alvo.hp - habilidade.dano;
+            // Debitar Mana
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo;
+                this.energia += 50;
+            }
+            // Debitar Energia
+            if (habilidade.energia > 0) {
+                this.energia = 0;
+            }
+        }
+        else {
+            return "Sem mana ou energia suficiente!";
+        }
+    }
+    boss_atacar(alvo) {
+        if (this.energia >= 100) {
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else {
+            this.energia += 50;
+        }
+    }
 }
 class Habilidade {
     constructor(id, nome, dano, custo, energia) {
@@ -39,6 +66,16 @@ let listaHabilidades = [
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");//<button>
     btn.innerText = hab.nome;
-    btn.id = `btn-${hab.id}`;
+    btn.classList.add("btn", "btn-outline-warning");
     containerBtn.appendChild(btn);
+    btn.onclick = () => {
+        hero.atacar_hero(boss, hab);
+        atualizarTela();
+    }
 });
+const atualizarTela = () => {
+    console.log(boss.hp);
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("en-hero").value = hero.energia;
+}
